d212638cd8fae86ff2c4b6b7ad1a554123c82244
Jothi <josphatkangethe01@gmail.com>
2023-07-26 13:52:41 +0300

    Update icon

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -3,7 +3,7 @@ import { Row, Col } from "react-bootstrap"
 import { StoreItem } from "../components/StoreItem"
 
 export function Store() {
-    return <><h1>Jothi's shop</h1>
+    return <><h1>online shop</h1>
     <Row md={2} xs={1} lg={3} className="g-3">
         {storeItems.map(item => (
             <Col key={item.id}><StoreItem{...item} /></Col>
